53531150e68f2ee7edb7dbbb1fbff9db746d20e1
JeHwanYoo <yoojehwan1875@gmail.com>
2025-01-05 02:22:27 +0900

    ✨feat(controller): SessionResponse.from_dict 메소드 구현
    Dto로 변환하기 위함

--- naver_smart_store_rag_chatbot/api/v1/sessions/sessions_response.py
+++ naver_smart_store_rag_chatbot/api/v1/sessions/sessions_response.py
@@ -1,7 +1,14 @@
 from dataclasses import dataclass
+from typing import Dict
 
 
 @dataclass
 class SessionResponse:
     session_id: str
     first_message: str
+
+    @staticmethod
+    def from_dict(data: Dict) -> 'SessionResponse':
+        return SessionResponse(
+            **data,
+        )
