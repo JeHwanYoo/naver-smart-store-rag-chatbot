31816a4f6c6bfb5eb1e70167ab4bdfd40591cd8c
JeHwanYoo <yoojehwan1875@gmail.com>
2025-01-05 00:27:34 +0900

    🧪test(e2e): GET /v1/sessions 테스트 작성

--- naver_smart_store_rag_chatbot/test_main.py
+++ naver_smart_store_rag_chatbot/test_main.py
@@ -1,3 +1,6 @@
+import uuid
+from datetime import datetime
+
 import pytest
 from fastapi.testclient import TestClient
 from motor.motor_asyncio import AsyncIOMotorClient
@@ -12,7 +15,38 @@ def http_client():
 
 @pytest.fixture(scope='module')
 def motor_client():
-    return AsyncIOMotorClient('mongodb://localhost:27017')
+    return AsyncIOMotorClient('mongodb://root:example@localhost:27017/')
+
+
+async def create_dummy_sessions(motor_client: AsyncIOMotorClient):
+    test_db = motor_client.get_database('test_db')
+    coll = test_db.get_collection('chat_histories')
+    chat_history_session_ids = [
+        str(uuid.uuid4()),
+        str(uuid.uuid4()),
+        str(uuid.uuid4()),
+    ]
+
+    chat_histories = []
+    for session_id in chat_history_session_ids:
+        for i in range(3):
+            chat_histories.append(
+                {
+                    'session_id': session_id,
+                    'created_at': datetime.now(),
+                    'user_message': 'fake user message',
+                    'system_message': 'fake system message',
+                    'recommends': ['fake recommends'],
+                }
+            )
+
+    await coll.insert_many(chat_histories)
+
+    return [{'session_id': session_id, 'first_message': 'fake user message'} for session_id in chat_history_session_ids]
+
+
+async def clear_test_db(motor_client: AsyncIOMotorClient):
+    await motor_client.drop_database('test_db')
 
 
 def test_root(http_client: TestClient):
@@ -25,3 +59,15 @@ def test_root(http_client: TestClient):
 async def test_motor(motor_client: AsyncIOMotorClient):
     ping = await motor_client.admin.command('ping')
     assert ping['ok'] == 1.0
+
+
+@pytest.mark.asyncio
+async def test_get_sessions(http_client: TestClient, motor_client: AsyncIOMotorClient):
+    response = http_client.get('/v1/sessions')
+
+    expect = await create_dummy_sessions(motor_client)
+
+    assert response.status_code == 200
+    assert response.json() == expect
+
+    await clear_test_db(motor_client)
