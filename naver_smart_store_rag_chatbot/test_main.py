8043c0be3c5201cd1109ffa305aac4fc21d41c9f
JeHwanYoo <yoojehwan1875@gmail.com>
2025-01-04 23:55:27 +0900

    🧪test(db): moto_client 테스트

--- naver_smart_store_rag_chatbot/test_main.py
+++ naver_smart_store_rag_chatbot/test_main.py
@@ -1,17 +1,27 @@
 import pytest
 from fastapi.testclient import TestClient
+from motor.motor_asyncio import AsyncIOMotorClient
 
 from .main import app
 
 
 @pytest.fixture(scope='module')
 def http_client():
-    with TestClient(app) as http_client:
-        yield http_client
-        http_client.close()
+    return TestClient(app)
+
+
+@pytest.fixture(scope='module')
+def motor_client():
+    return AsyncIOMotorClient('mongodb://localhost:27017')
 
 
 def test_root(http_client: TestClient):
     response = http_client.get('/v1')
     assert response.status_code == 200
     assert response.text == '"OK"'
+
+
+@pytest.mark.asyncio
+async def test_motor(motor_client: AsyncIOMotorClient):
+    ping = await motor_client.admin.command('ping')
+    assert ping['ok'] == 1.0
