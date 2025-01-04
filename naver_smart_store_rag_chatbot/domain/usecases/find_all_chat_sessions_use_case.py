d7be37691511c396a3046917d24d2b35334e7b69
JeHwanYoo <yoojehwan1875@gmail.com>
2025-01-05 00:44:12 +0900

    ✨feat(domain): FindAllChatSessionsUseCase.execute() 구현

--- naver_smart_store_rag_chatbot/domain/usecases/find_all_chat_sessions_use_case.py
+++ naver_smart_store_rag_chatbot/domain/usecases/find_all_chat_sessions_use_case.py
@@ -4,3 +4,6 @@ from naver_smart_store_rag_chatbot.domain.interfaces.chat_repository import Chat
 class FindAllChatSessionsUseCase:
     def __init__(self, chat_session_repository: ChatRepository):
         self.chat_session_repository = chat_session_repository
+
+    def execute(self):
+        return self.chat_session_repository.find_all()
