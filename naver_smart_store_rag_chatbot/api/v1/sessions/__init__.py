fe986a2376278cda4a4caf9b66abc8708d2fea2e
JeHwanYoo <yoojehwan1875@gmail.com>
2025-01-04 22:46:26 +0900

    ✨feat(controller): POST v1/sessions/{session_id}/chats 명세 작성

--- naver_smart_store_rag_chatbot/api/v1/sessions/__init__.py
+++ naver_smart_store_rag_chatbot/api/v1/sessions/__init__.py
@@ -4,9 +4,18 @@ from typing import List
 from fastapi import APIRouter
 
 from naver_smart_store_rag_chatbot.api.v1.sessions.chats_response import ChatResponse
+from naver_smart_store_rag_chatbot.api.v1.sessions.send_user_message_request import SendUserMessageRequest
+from naver_smart_store_rag_chatbot.api.v1.sessions.send_user_message_response import SendUserMessageResponse
 from naver_smart_store_rag_chatbot.api.v1.sessions.sessions_response import SessionResponse
 
 sessions_router = APIRouter(prefix='/sessions')
+
+
+@sessions_router.post(
+    '/{session_id}/chats', description='특정 세션에 유저 메세지를 보냅니다. 스트리밍 ID를 반환 받습니다.'
+)
+async def send_user_message(session_id: str, _: SendUserMessageRequest) -> SendUserMessageResponse:
+    return SendUserMessageResponse(session_id=session_id, streaming_id=str(uuid.uuid4()))
 
 
 @sessions_router.get('/{session_id}/chats', description='특정 세션의 대화 목록을 불러옵니다.')
