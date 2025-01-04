42f94ae7dce26c9d591d426cf6ef56d1996441a8
JeHwanYoo <yoojehwan1875@gmail.com>
2025-01-05 01:53:56 +0900

    ✨feat(infra): DI 선언

--- naver_smart_store_rag_chatbot/main.py
+++ naver_smart_store_rag_chatbot/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
 
 from naver_smart_store_rag_chatbot.api.v1 import v1_router
+from naver_smart_store_rag_chatbot.infrastructure.di_container import Container
+from naver_smart_store_rag_chatbot.infrastructure.repositories.mongo_chat_session_repository import (
+    MongoChatSessionRepository,
+)
 
 app = FastAPI()
 
 app.include_router(v1_router)
+
+# DI
+container = Container(
+    chat_session_repository=MongoChatSessionRepository(),
+)
+container.wire(
+    modules=[
+        'naver_smart_store_rag_chatbot.api.v1.sessions',
+    ]
+)
