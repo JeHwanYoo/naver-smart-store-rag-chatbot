1cc2a1039f3898c2e4eb07da62b0f1d27121f876
JeHwanYoo <yoojehwan1875@gmail.com>
2025-01-05 13:34:22 +0900

    feat(infra): CachetoolsLLMQueueService 구현체 정의

---
+++ naver_smart_store_rag_chatbot/infrastructure/services/cachetools_llm_queue_service.py
@@ -0,0 +1,18 @@
+import asyncio
+from uuid import uuid4
+
+from cachetools import TTLCache
+
+from naver_smart_store_rag_chatbot.domain.interfaces.services.llm_queue_service import LLMQueueService
+
+
+class CachetoolsLLMQueueService(LLMQueueService):
+    def __init__(self, maxsize: int = 1024, ttl: int = 60):
+        self.cache = TTLCache(maxsize, ttl=ttl)
+        self.lock = asyncio.Lock()
+
+    async def add(self, session_id: str, user_message: str) -> str:
+        streaming_id = uuid4()
+        async with self.lock:
+            self.cache[session_id] = (str(streaming_id), user_message)
+        return str(streaming_id)
