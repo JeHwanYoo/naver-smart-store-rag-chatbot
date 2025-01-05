63bd6be0c7ec7e51342d3fca7fbe7f2f0da9a518
JeHwanYoo <yoojehwan1875@gmail.com>
2025-01-05 10:30:14 +0900

    ✨feat(infra): find_chats_by_session_id_use_case 를 DI 컨테이너에 선언

--- naver_smart_store_rag_chatbot/infrastructure/di_container.py
+++ naver_smart_store_rag_chatbot/infrastructure/di_container.py
@@ -1,13 +1,20 @@
 from dependency_injector import containers, providers
 
+from naver_smart_store_rag_chatbot.domain.interfaces.chat_repository import ChatRepository
 from naver_smart_store_rag_chatbot.domain.interfaces.chat_session_repository import ChatSessionRepository
 from naver_smart_store_rag_chatbot.domain.usecases.find_all_chat_sessions_use_case import FindAllChatSessionsUseCase
 
 
 class Container(containers.DeclarativeContainer):
     chat_session_repository = providers.Dependency(instance_of=ChatSessionRepository)
+    chat_repository = providers.Dependency(instance_of=ChatRepository)
 
     find_all_chat_sessions_use_case = providers.Factory(
         FindAllChatSessionsUseCase,
         chat_session_repository=chat_session_repository,
+    )
+
+    find_chats_by_session_id_use_case = providers.Factory(
+        FindAllChatSessionsUseCase,
+        chat_repository=chat_repository,
     )
