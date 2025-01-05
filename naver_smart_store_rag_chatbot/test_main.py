b8a04d77380980e4bc5a3320f10c542eedfc0eb8
JeHwanYoo <yoojehwan1875@gmail.com>
2025-01-05 11:31:59 +0900

    ♻️refactor(e2e): dummy_chats_in_session 응답을 MongoDB로 생성

--- naver_smart_store_rag_chatbot/test_main.py
+++ naver_smart_store_rag_chatbot/test_main.py
@@ -79,14 +79,9 @@ async def dummy_sessions(motor_client: AsyncIOMotorClient):
 async def dummy_chats_in_session(motor_client: AsyncIOMotorClient):
     chat_histories = await create_dummy_chat_histories(motor_client)
     session_id = chat_histories[0]['session_id']
-
-    results = [history for history in chat_histories if history['session_id'] == session_id]
-    results.sort(key=lambda x: x['created_at'])
-
-    return [
-        {'session_id': r['session_id'], 'user_message': r['user_message'], 'system_message': r['system_message']}
-        for r in results
-    ]
+    test_db = motor_client.get_database('test_db')
+    coll = test_db.get_collection('chat_histories')
+    return await coll.find({'session_id': session_id}).sort('created_at', 1).to_list(length=None)
 
 
 async def clear_test_db(motor_client: AsyncIOMotorClient):
