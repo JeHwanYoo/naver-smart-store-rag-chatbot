959875ef0dc708dc978e75482a8583dc70adc6f9
JeHwanYoo <yoojehwan1875@gmail.com>
2025-01-05 10:32:56 +0900

    ✨feat(controller): get_chats_by_session_id 구현

--- naver_smart_store_rag_chatbot/api/v1/sessions/__init__.py
+++ naver_smart_store_rag_chatbot/api/v1/sessions/__init__.py
@@ -10,6 +10,7 @@ from naver_smart_store_rag_chatbot.api.v1.sessions.send_user_message_request imp
 from naver_smart_store_rag_chatbot.api.v1.sessions.send_user_message_response import SendUserMessageResponse
 from naver_smart_store_rag_chatbot.api.v1.sessions.sessions_response import SessionResponse
 from naver_smart_store_rag_chatbot.domain.usecases.find_all_chat_sessions_use_case import FindAllChatSessionsUseCase
+from naver_smart_store_rag_chatbot.domain.usecases.find_chats_by_session_id_use_case import FindChatsBySessionIdUseCase
 from naver_smart_store_rag_chatbot.infrastructure.di_container import Container
 
 sessions_router = APIRouter(prefix='/sessions')
@@ -23,8 +24,14 @@ async def send_user_message(session_id: str, _: SendUserMessageRequest) -> SendU
 
 
 @sessions_router.get('/{session_id}/chats', description='특정 세션의 대화 목록을 불러옵니다.')
-async def get_chats_by_session_id(session_id: str) -> List[ChatResponse]:
-    return [ChatResponse(session_id=session_id, user_message='fake question', system_message='fake answer')]
+@inject
+async def get_chats_by_session_id(
+    session_id: str,
+    find_chats_by_session_id_use_case: FindChatsBySessionIdUseCase = Depends(
+        Provide[Container.find_chats_by_session_id_use_case]
+    ),
+) -> List[ChatResponse]:
+    return [ChatResponse.from_dict(x) for x in await find_chats_by_session_id_use_case.execute(session_id)]
 
 
 @sessions_router.get('/{session_id}/recommends', description='가장 최근 대화의 추천 질문 목록을 받습니다. (3개)')
