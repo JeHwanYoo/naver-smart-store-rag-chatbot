0d1c92c9bd24dcf95e5703e4cf42d31a8484a9a9
JeHwanYoo <yoojehwan1875@gmail.com>
2025-01-05 11:18:23 +0900

    ✨feat(infra): MongoChatRepository 의존성 주입

--- naver_smart_store_rag_chatbot/main.py
+++ naver_smart_store_rag_chatbot/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from naver_smart_store_rag_chatbot.api.v1 import v1_router
 from naver_smart_store_rag_chatbot.infrastructure.di_container import Container
+from naver_smart_store_rag_chatbot.infrastructure.repositories.mongo_chat_repository import MongoChatRepository
 from naver_smart_store_rag_chatbot.infrastructure.repositories.mongo_chat_session_repository import (
     MongoChatSessionRepository,
 )
@@ -11,9 +12,7 @@ app = FastAPI()
 app.include_router(v1_router)
 
 # DI
-container = Container(
-    chat_session_repository=MongoChatSessionRepository(),
-)
+container = Container(chat_session_repository=MongoChatSessionRepository(), chat_repository=MongoChatRepository())
 container.wire(
     modules=[
         'naver_smart_store_rag_chatbot.api.v1.sessions',
