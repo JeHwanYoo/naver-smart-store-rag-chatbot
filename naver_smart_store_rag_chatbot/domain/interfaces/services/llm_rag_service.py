1973aa35d1573d6695aa84c984fd387348cf6929
JeHwanYoo <yoojehwan1875@gmail.com>
2025-01-05 16:24:34 +0900

    ✨feat(domain): StreamingSystemMessageUseCase.execute() 구현

--- naver_smart_store_rag_chatbot/domain/interfaces/services/llm_rag_service.py
+++ naver_smart_store_rag_chatbot/domain/interfaces/services/llm_rag_service.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import AsyncGenerator, List
+from typing import List, Generator
 
 from naver_smart_store_rag_chatbot.domain.entities.chat import Chat
 from naver_smart_store_rag_chatbot.domain.entities.document import Document
@@ -7,7 +7,7 @@ from naver_smart_store_rag_chatbot.domain.entities.document import Document
 
 class LLMRagService(ABC):
     @abstractmethod
-    async def send_question(
+    def send_question(
         self, user_message: str, related_documents: List[Document], recent_chats: List[Chat]
-    ) -> AsyncGenerator:
+    ) -> Generator:
         pass
--- naver_smart_store_rag_chatbot/domain/usecases/streaming_system_message_use_case.py
+++ naver_smart_store_rag_chatbot/domain/usecases/streaming_system_message_use_case.py
@@ -1,20 +1,35 @@
 from naver_smart_store_rag_chatbot.domain.interfaces.repositories.chat_repository import ChatRepository
 from naver_smart_store_rag_chatbot.domain.interfaces.services.llm_queue_service import LLMQueueService
+from naver_smart_store_rag_chatbot.domain.interfaces.services.llm_rag_service import LLMRagService
+from naver_smart_store_rag_chatbot.domain.interfaces.services.vector_db_service import VectorDBService
 
 
 class StreamingSystemMessageUseCase:
     def __init__(
-        self, chat_repository: ChatRepository, llm_queue_service: LLMQueueService, vector_db_service, llm_rag_service
+        self,
+        chat_repository: ChatRepository,
+        llm_queue_service: LLMQueueService,
+        vector_db_service: VectorDBService,
+        llm_rag_service: LLMRagService,
     ):
         self.chat_repository = chat_repository
         self.llm_queue_service = llm_queue_service
         self.vector_db_service = vector_db_service
         self.llm_rag_service = llm_rag_service
 
-    def execute(self, streaming_id: str):
+    async def execute(self, streaming_id: str):
         task = self.llm_queue_service.get(streaming_id)
 
         if not task:
             return None
 
-        pass
+        session_id, user_message = task
+
+        related_documents = await self.vector_db_service.find_related_documents(user_message=user_message)
+        recent_chats = await self.chat_repository.find_recent_chats(session_id)
+
+        return self.llm_rag_service.send_question(
+            user_message,
+            related_documents=related_documents,
+            recent_chats=recent_chats,
+        )
