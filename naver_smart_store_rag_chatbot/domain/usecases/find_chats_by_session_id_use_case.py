38bf872c4023b8e14cb419d95066f4a410d6ed52
JeHwanYoo <yoojehwan1875@gmail.com>
2025-01-05 10:28:59 +0900

    ✨feat(domain): FindChatsBySessionIdUseCase.execute() 구현

--- naver_smart_store_rag_chatbot/domain/usecases/find_chats_by_session_id_use_case.py
+++ naver_smart_store_rag_chatbot/domain/usecases/find_chats_by_session_id_use_case.py
@@ -1,3 +1,9 @@
+from naver_smart_store_rag_chatbot.domain.interfaces.chat_repository import ChatRepository
+
+
 class FindChatsBySessionIdUseCase:
-    def __init__(self, chat_repository):
+    def __init__(self, chat_repository: ChatRepository):
         self.chat_repository = chat_repository
+
+    def execute(self, session_id: str):
+        return self.chat_repository.find_by_session_id(session_id)
