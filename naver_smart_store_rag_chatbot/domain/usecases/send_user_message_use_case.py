18227cf09df88d93b72d5bab44cc783fcca4d139
JeHwanYoo <yoojehwan1875@gmail.com>
2025-01-05 13:04:32 +0900

    ✨feat(domain): SendUserMessageUseCase 에서 streaming_id 반환 구현

--- naver_smart_store_rag_chatbot/domain/usecases/send_user_message_use_case.py
+++ naver_smart_store_rag_chatbot/domain/usecases/send_user_message_use_case.py
@@ -1,14 +1,16 @@
 from naver_smart_store_rag_chatbot.domain.interfaces.repositories.chat_repository import ChatRepository
+from naver_smart_store_rag_chatbot.domain.interfaces.services.llm_queue_service import LLMQueueService
 
 
 class SendUserMessageUseCase:
-    def __init__(self, chat_repository: ChatRepository):
+    def __init__(self, chat_repository: ChatRepository, llm_queue_service: LLMQueueService):
         self.chat_repository = chat_repository
+        self.llm_queue_service = llm_queue_service
 
     async def execute(self, session_id: str, user_message: str):
         chats = await self.chat_repository.find_by_session_id(session_id)
 
         if len(chats) > 0:
-            pass
+            return await self.llm_queue_service.add(session_id, user_message)
         else:
             return None
