d59d2a249cc8e00ef60360edb55101e2777ec392
JeHwanYoo <yoojehwan1875@gmail.com>
2025-01-05 16:31:36 +0900

    ✨feat(controller): stream_system_message 구현

--- naver_smart_store_rag_chatbot/api/v1/streaming/__init__.py
+++ naver_smart_store_rag_chatbot/api/v1/streaming/__init__.py
@@ -1,4 +1,11 @@
-from fastapi import APIRouter
+from dependency_injector.wiring import Provide
+from fastapi import APIRouter, Request, Depends
+
+from naver_smart_store_rag_chatbot.domain.usecases.streaming_system_message_use_case import (
+    StreamingSystemMessageUseCase,
+)
+from naver_smart_store_rag_chatbot.infrastructure.di_container import Container
+from fastapi.responses import StreamingResponse
 
 streaming_router = APIRouter(prefix='/streaming')
 
@@ -6,5 +13,17 @@ streaming_router = APIRouter(prefix='/streaming')
 @streaming_router.post(
     '/{streaming_id}', description='특정 스트리밍 id를 이용하여 답변에 대한 스트리밍을 받습니다 (SSE)'
 )
-async def stream_system_message(streaming_id: str):
-    pass
+async def stream_system_message(
+    request: Request,
+    streaming_id: str,
+    streaming_system_message_use_case: StreamingSystemMessageUseCase = Depends(
+        Provide[Container.streaming_system_message_use_case]
+    ),
+):
+    async def event_generator():
+        for chunk in await streaming_system_message_use_case.execute(streaming_id):
+            if await request.is_disconnected():
+                break
+            yield chunk
+
+    return StreamingResponse(event_generator(), media_type='text/event-stream')
