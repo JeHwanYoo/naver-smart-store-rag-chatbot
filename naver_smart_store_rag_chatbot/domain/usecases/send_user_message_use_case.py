35f947d5aba3b1d909ee67e18544e73708723634
JeHwanYoo <yoojehwan1875@gmail.com>
2025-01-05 12:57:58 +0900

    ✨feat(domain): SendUserMessageUseCase.execute() 세션 조회 부분 구현

--- naver_smart_store_rag_chatbot/domain/usecases/send_user_message_use_case.py
+++ naver_smart_store_rag_chatbot/domain/usecases/send_user_message_use_case.py
@@ -4,3 +4,11 @@ from naver_smart_store_rag_chatbot.domain.interfaces.chat_repository import Chat
 class SendUserMessageUseCase:
     def __init__(self, chat_repository: ChatRepository):
         self.chat_repository = chat_repository
+
+    async def execute(self, session_id: str, user_message: str):
+        chats = await self.chat_repository.find_by_session_id(session_id)
+
+        if len(chats) > 0:
+            pass
+        else:
+            return None
