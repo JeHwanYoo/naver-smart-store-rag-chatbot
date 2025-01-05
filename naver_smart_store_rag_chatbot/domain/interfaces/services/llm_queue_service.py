cc8880ce8cfa3a6060879e5f6d4bb03da5a04625
JeHwanYoo <yoojehwan1875@gmail.com>
2025-01-05 15:46:48 +0900

    ✨feat(domain): LLMQueueService.get() 인터페이스 정의

--- naver_smart_store_rag_chatbot/domain/interfaces/services/llm_queue_service.py
+++ naver_smart_store_rag_chatbot/domain/interfaces/services/llm_queue_service.py
@@ -1,7 +1,12 @@
 from abc import ABC, abstractmethod
+from typing import Optional, Tuple
 
 
 class LLMQueueService(ABC):
     @abstractmethod
     async def add(self, session_id: str, user_message: str) -> str:
+        pass
+
+    @abstractmethod
+    async def get(self, streaming_id: str) -> Optional[Tuple[str, str]]:
         pass
