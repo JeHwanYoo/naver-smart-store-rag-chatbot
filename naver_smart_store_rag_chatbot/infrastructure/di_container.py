49807626a082d04bf7df5c66bea3d8fb3ad01f40
JeHwanYoo <yoojehwan1875@gmail.com>
2025-01-05 13:34:03 +0900

    feat(infra): send_user_message_use_case 를 di 컨테이너에 선언

--- naver_smart_store_rag_chatbot/infrastructure/di_container.py
+++ naver_smart_store_rag_chatbot/infrastructure/di_container.py
@@ -2,13 +2,16 @@ from dependency_injector import containers, providers
 
 from naver_smart_store_rag_chatbot.domain.interfaces.repositories.chat_repository import ChatRepository
 from naver_smart_store_rag_chatbot.domain.interfaces.repositories.chat_session_repository import ChatSessionRepository
+from naver_smart_store_rag_chatbot.domain.interfaces.services.llm_queue_service import LLMQueueService
 from naver_smart_store_rag_chatbot.domain.usecases.find_all_chat_sessions_use_case import FindAllChatSessionsUseCase
 from naver_smart_store_rag_chatbot.domain.usecases.find_chats_by_session_id_use_case import FindChatsBySessionIdUseCase
+from naver_smart_store_rag_chatbot.domain.usecases.send_user_message_use_case import SendUserMessageUseCase
 
 
 class Container(containers.DeclarativeContainer):
     chat_session_repository = providers.Dependency(instance_of=ChatSessionRepository)
     chat_repository = providers.Dependency(instance_of=ChatRepository)
+    llm_queue_service = providers.Dependency(instance_of=LLMQueueService)
 
     find_all_chat_sessions_use_case = providers.Factory(
         FindAllChatSessionsUseCase,
@@ -18,4 +21,10 @@ class Container(containers.DeclarativeContainer):
     find_chats_by_session_id_use_case = providers.Factory(
         FindChatsBySessionIdUseCase,
         chat_repository=chat_repository,
+    )
+
+    send_user_message_use_case = providers.Factory(
+        SendUserMessageUseCase,
+        chat_repository=chat_repository,
+        llm_queue_service=llm_queue_service,
     )
